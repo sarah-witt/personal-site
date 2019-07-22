01a10ba881315e22c6efad3662e0e13b5390f721
Sarah Witt <witt.s@husky.neu.edu>
2019-07-22 01:06:54 -0400

    Add basic logos, used a lot from my footer work on sandboxneu

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,14 @@
 import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons"
+import {
+  faEnvelope
+} from "@fortawesome/free-solid-svg-icons"
 
 const StyledHeader = styled.div`
   font-size: 4rem;
@@ -16,8 +24,36 @@ const StyledDescription = styled.div`
   font-size: 1.5rem;
   padding: 5%;
   color: #4d8a76;
-
 `
+
+const StyledLogo = styled(FontAwesomeIcon)`
+  font-size: 1.5em;
+  color: #fff0b5;
+
+  &:hover {
+    color: white;
+  }
+`
+
+const SocialSection = styled.div`
+  padding-top: 10px;
+  display: flex;
+  justify-content: center;
+`
+const StyledSocial = styled.div`
+  padding-right: 1%;
+  padding-left: 1%;
+`
+
+const ContactLink = ({ link, icon }) => {
+  return (
+    <StyledSocial> 
+      <a href={link}>
+        <StyledLogo icon={icon}> </StyledLogo>
+      </a>
+    </StyledSocial>
+  )
+}
 
 const Header = ({ siteTitle, siteDescription }) => (
   <TitleSection>
@@ -27,6 +63,11 @@ const Header = ({ siteTitle, siteDescription }) => (
     <StyledDescription>
       {siteDescription}
     </StyledDescription>
+    <SocialSection> 
+      <ContactLink link={"https://github.com/sarah-witt"} icon={faGithub} ></ContactLink> 
+      <ContactLink link={"mailto:witt.s@husky.neu.edu"} icon={faEnvelope} ></ContactLink> 
+      <ContactLink link={"https://www.linkedin.com/in/sarah-witt/"} icon={faLinkedin}></ContactLink>
+    </SocialSection>
   </TitleSection>
 )
 
